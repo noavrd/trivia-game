ce334b154588309babe48a2c2b29f22e00ac2058
Shira-Meirovitz <shirameirovi@gmail.com>
2021-04-25 00:50:45 +0300

    change question by click

--- trivia/src/components/Game.js
+++ trivia/src/components/Game.js
@@ -3,33 +3,43 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function Game() {
-  const findRightAnswer = (option) => {
-    if (answer !== '') {
-      if (option === answer) {
-        console.log(1);
-        return true;
-      }
-      console.log(2);
-
-      return false;
-    }
-  };
-  const [options, setOptions] = useState([]);
-  const [question, setQuestion] = useState('');
-
-  const [answer, setAnswer] = useState('');
-  useEffect(() => {
+  const getQuestion = () => {
     axios
       .get('questions')
       .then((result) => {
         setQuestion(result.data.question);
         setOptions(result.data.options);
         setAnswer(result.data.options[0]);
-        console.log(4);
       })
       .catch((err) => console.log(err));
+  };
+  const findRightAnswer = (option) => {
+    if (answer !== '') {
+      if (option === answer) {
+        return true;
+      }
+      return false;
+    }
+  };
+  const [options, setOptions] = useState([]);
+  const [question, setQuestion] = useState('');
+  const [score, setScore] = useState(0);
+
+  const [answer, setAnswer] = useState('');
+  useEffect(() => {
+    getQuestion();
   }, []);
-  console.log(5);
+
+  //add score by your answer
+  const rightAnswer = () => {
+    getQuestion();
+    return setScore(score + 100);
+  };
+  const wrongAnswer = () => {
+    getQuestion();
+    return setScore(score);
+  };
+  console.log(question);
   console.log(answer);
   console.log(options);
   return (
@@ -37,7 +47,7 @@ export default function Game() {
       <h1 className="generalHeadline">World Trivia</h1>
 
       <div className="game-page">
-        <div>Question</div>
+        <div>score: {score}</div>
         <div>{question}</div>
 
         {randomOptions(options).map((option, i) =>
@@ -55,15 +65,8 @@ export default function Game() {
     </div>
   );
 }
-const rightAnswer = () => {
-  return console.log('right answer');
-};
-const wrongAnswer = () => {
-  return console.log('wrong answer');
-};
 
 const randomOptions = (options) => {
   let shuffled = options.sort(() => 0.5 - Math.random());
-  console.log(3);
   return shuffled;
 };
