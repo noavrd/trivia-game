6668613d92085137839dba0384dfa8681784009a
Shira-Meirovitz <shirameirovi@gmail.com>
2021-04-25 21:46:11 +0300

    have to ender username to start

--- trivia/src/components/Home.js
+++ trivia/src/components/Home.js
@@ -1,25 +1,40 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-    const [userName, setUserName] = useState("");
-    return (
-        <div className="home">
-            <h1 className="homeHeadline">
-            World Trivia
-            </h1>
-            <form>
-                {/* <label>User Name</label><br/> */}
-                <input placeholder="Enter user name"></input><br/>
-                <Link
-                to= {{pathname: '/game'}}>
-                    <button className="start-btn">START</button>
-                </Link> 
-            </form>
-            <Link
-            to= {{pathname: '/leaderboard'}}>
-                <button className="leaderboard-btn">Leader Board</button>
-            </Link>
-        </div>
-    )
+  const [userName, setUserName] = useState('');
+  const [isErr, setIsErr] = useState(false);
+
+  const login = (e) => {
+    if (userName === '') {
+      e.preventDefault();
+      setIsErr(true);
+    } else {
+      setIsErr(false);
+    }
+  };
+
+  return (
+    <div className="home">
+      <h1 className="homeHeadline">World Trivia</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="Enter user name"
+          onChange={(e) => setUserName(e.target.value)}
+          required
+        ></input>
+        <br />
+        <Link to={{ pathname: '/game' }}>
+          <button onClick={(e) => login(e)} className="start-btn">
+            START
+          </button>
+        </Link>
+        <div>{isErr ? 'Please enter UserName' : ''}</div>
+      </form>
+      <Link to={{ pathname: '/leaderboard' }}>
+        <button className="leaderboard-btn">Leader Board</button>
+      </Link>
+    </div>
+  );
 }
