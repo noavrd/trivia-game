117333dad740f290ce0d100c08a31b62516bfbba
Shira-Meirovitz <shirameirovi@gmail.com>
2021-04-25 22:52:20 +0300

    finished showing the leaderBoard

--- trivia/src/components/LeaderBoard.js
+++ trivia/src/components/LeaderBoard.js
@@ -1,16 +1,37 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 export default function LeaderBoard() {
-    return (
-        <div>
-            <h1 className="generalHeadline">
-            World Trivia
-            </h1>
-            <div className="leaderboard-page">
-                <table>
-                        <th>Rank</th>
-                        <th>Player</th>
-                        <th>Score</th>
-                </table>
-            </div>
-        </div>
-    )
+  const [leaders, setLeaders] = useState([]);
+  useEffect(() => {
+    axios
+      .get('leaderboard')
+      .then((result) => {
+        setLeaders(result.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(leaders);
+
+  return (
+    <div>
+      <h1 className="generalHeadline">World Trivia</h1>
+      <div className="leaderboard-page">
+        <table>
+          <tr>
+            <th>Rank</th>
+            <th>Player</th>
+            <th>Score</th>
+          </tr>
+
+          {leaders.map((leader, i) => (
+            <tr>
+              <td>{i}</td>
+              <td>{leader.user_name}</td>
+              <td>{leader.score}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </div>
+  );
 }
